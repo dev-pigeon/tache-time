89fdfedeec1272eed738f2fdf76ae4bc355cbca9
Jack Young <jdydev2003@gmail.com>
2025-05-18 17:15:19 -0600

    returns a valid port when input is within range

--- server/src/tests/findServerPort.test.js
+++ server/src/tests/findServerPort.test.js
@@ -8,4 +8,9 @@ describe("Can find a valid port", () => {
    test("findServerPort does not throw error for input within valid range", () => {
     expect(() => util.findServerPort(8080)).not.toThrow();
    })
+
+   test("findServerPort returns valid port", () => {
+    const port = util.findServerPort(8080);
+    expect(port).toBeGreaterThanOrEqual(8080);
+   })
 })
