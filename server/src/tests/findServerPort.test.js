afb866fede26b6577ffc6f2c4fe16c5ec1c27396
Jack Young <jdydev2003@gmail.com>
2025-05-18 17:12:58 -0600

    does not throw error for correct input range

--- server/src/tests/findServerPort.test.js
+++ server/src/tests/findServerPort.test.js
@@ -4,4 +4,8 @@ describe("Can find a valid port", () => {
     test("findServerPort rejects restricted port", () => {
         expect(() => util.findServerPort(1)).toThrow();
     })
+
+   test("findServerPort does not throw error for input within valid range", () => {
+    expect(() => util.findServerPort(8080)).not.toThrow();
+   })
 })
