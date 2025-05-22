6fe74ca269728532810e8b0b9f03b8be362f5b0d
Jack Young <jdydev2003@gmail.com>
2025-05-22 16:28:12 -0600

    added interleave test

--- server/src/tests/TaskScheduler.test.js
+++ server/src/tests/TaskScheduler.test.js
@@ -49,16 +49,41 @@ describe("TaskScheduler correctly schedules tasks", () => {
   let scheduler;
   let task1;
   let task2;
+  let task3;
+  let task4;
+  let task5;
 
   beforeEach(() => {
     scheduler = new TaskScheduler();
     task1 = new Task("Task1", "t1", 3, 2);
     task2 = new Task("Task2", "t2", 5, 3);
+    task3 = new Task("Task3", "t3", 6, 3);
+    task4 = new Task("Task4", "t4", 6, 2);
+    task5 = new Task("Task5", "t5", 9, 3);
   });
 
   test("Simple two task scheduling", () => {
     const taskList = [task1, task2];
     const expectedOrder = ["Task1", "Task1", "Task2", "Task2", "Task2"];
+    const workUnits = scheduler.scheduleTasks(taskList);
+    for (let i = 0; i < workUnits.length; ++i) {
+      const unit = workUnits[i];
+      expect(unit.name).toBe(expectedOrder[i]);
+    }
+  });
+
+  test("Interleaving tasks", () => {
+    const taskList = [task3, task4, task5];
+    const expectedOrder = [
+      "Task3",
+      "Task3",
+      "Task4",
+      "Task 3",
+      "Task4",
+      "Task5",
+      "Task5",
+      "Task5",
+    ];
     const workUnits = scheduler.scheduleTasks(taskList);
     for (let i = 0; i < workUnits.length; ++i) {
       const unit = workUnits[i];
