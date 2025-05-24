20521588bb1b6ae6c5c787ac64143bcdf6e3df4b
Jack Young <jdydev2003@gmail.com>
2025-05-24 14:34:47 -0600

    palette vars accessible to css files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./styles/Theme.tsx";
+import "./styles/index.css";
+import App from "./App.tsx";
+import { ThemeVariablesProvider } from "./styles/ThemeVariablesProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <ThemeVariablesProvider>
+        <App />
+      </ThemeVariablesProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
