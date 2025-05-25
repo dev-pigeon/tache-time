c0ed56dfeeef66ead125f13a9cb3f0f287a8aca7
Jack Young <jdydev2003@gmail.com>
2025-05-25 14:15:19 -0600

    created use calendar container

---
+++ client/src/hooks/useCalendarContainer.ts
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+interface useCalendarContainerReturnProps {
+    initializeDates : () => void;
+    dates : string[] | undefined;
+}
+
+const useCalendarContainer = () : useCalendarContainerReturnProps => {
+    const [dates, setDates] = useState<string[] | undefined>(undefined)
+
+    const initializeDates = () => {
+        const date : Date = new Date();
+        let newDates : string[] = [];
+        for(let i = 0; i < 7; ++i) {
+            const dateString = getDateString(date);
+            newDates.push(dateString);
+        }
+        setDates(dates);
+    }
+
+    const getDateString = (date : Date) : string => {
+        const monthString : string = getMonthString(date);
+        const dayOfMonth : number = date.getDate();
+        const oridinalSuffix = getDateOrdinalSuffix(dayOfMonth);
+        return `${monthString} ${dayOfMonth}${oridinalSuffix}`;
+    }
+
+    const getMonthString = (date : Date) : string => {
+        const months = ["January", "February","March","April","May","June","July","August","September","October","November","December"];
+        const monthIndex = date.getMonth();
+        return months[monthIndex];
+    }
+
+    const getDateOrdinalSuffix = (dayOfMonth : number) : string => {
+        if(dayOfMonth >= 11 && dayOfMonth <= 13) return "th"
+        const modulus = dayOfMonth % 10;
+        switch(modulus) {
+            case 1:
+                return "st";
+            case 2: 
+                return "nd";
+            case 3: return "rd"
+        }
+        return "th";
+    }
+
+    return {
+        initializeDates,
+        dates,
+    }
+}
+
+export default useCalendarContainer;
