0ef7315130c2470b1ada07e64abddfccce8535a7
Jack Young <jdydev2003@gmail.com>
2025-05-26 14:11:31 -0600

    added another tet for init dates

--- client/src/test/useCalendarContainer.test.ts
+++ client/src/test/useCalendarContainer.test.ts
@@ -2,9 +2,10 @@ import {renderHook, act} from "@testing-library/react";
 import useCalendarContainer from "../hooks/useCalendarContainer";
 
 describe("initialize dates works as expected", () => {
-    const calendarContainerHook = renderHook(() => useCalendarContainer()).result;
+    
 
     test("correctly outputs all 'th' dates", () => {
+        const calendarContainerHook = renderHook(() => useCalendarContainer()).result;
         const expected = ["May 10th", "May 11th", "May 12th", "May 13th", "May 14th", "May 15th", "May 16th"];
         const date = new Date(2025, 4, 10);
         act(() => {
@@ -16,4 +17,16 @@ describe("initialize dates works as expected", () => {
         }
     })
 
+    test("can properly set `nd` `st` and `rd` dates", () => {
+        const calendarContainerHook = renderHook(() => useCalendarContainer()).result;
+        const expected = ["May 1st", "May 2nd", "May 3rd", "May 4th", "May 5th", "May 6th", "May 7th"];
+        const firstDate = new Date(2025, 4, 1);
+        act(() => {
+            calendarContainerHook.current.initializeDates(firstDate)
+        })
+         expect(calendarContainerHook.current.dates).toBeDefined()
+        for(let i = 0; i < expected.length; ++i) {
+            expect(calendarContainerHook.current.dates![i]).toBe(expected[i])
+        }
+    })
 })
