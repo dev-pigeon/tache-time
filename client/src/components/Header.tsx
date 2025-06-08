eaf357dc04f4ec8bd8ddb04a7f9b004b661f1ae6
Jack Young <jdydev2003@gmail.com>
2025-06-07 21:02:35 -0500

    added title to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,13 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import "../styles/Header.css";
 
 const Header = () => {
-  return <Box id={"header"}></Box>;
+  return (
+    <Box id={"header"}>
+      <Typography id={"header-title"}>Tâche Time</Typography>
+    </Box>
+  );
 };
 
 export default Header;
