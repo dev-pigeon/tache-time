c5136bf82bfd90af28fd1b9f4aaded5ef3c2647c
Jack Young <jdydev2003@gmail.com>
2025-06-07 21:01:53 -0500

    fixed header to top with styles for title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import CalendarContainer from "./components/CalendarContainer";
+import Header from "./components/Header";
 import "./styles/App.css";
 
 function App() {
   return (
     <div id="outer-container">
+      <Header />
       <CalendarContainer />
     </div>
   );
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@mui/material";
 
+import "../styles/Header.css";
+
 const Header = () => {
   return <Box id={"header"}></Box>;
 };
