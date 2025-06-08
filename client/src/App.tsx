b4147cc7ee3994e545126031b71bc9ed84375b81
Jack Young <jdydev2003@gmail.com>
2025-06-08 16:40:19 -0500

    sorted view control components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import CalendarContainer from "./components/calendar/CalendarContainer";
 import Header from "./components/Header";
 import TaskWidgetContainer from "./components/TaskOptions/TaskWidgetContainer";
-import ViewControlContainer from "./components/ViewControlContainer";
+import ViewControlContainer from "./components/ViewControl/ViewControlContainer";
 import "./styles/App.css";
 
 function App() {
