9f0d9a02862329ec4c54a1d60034b9843769b8bc
Jack Young <jdydev2003@gmail.com>
2025-06-07 21:57:04 -0500

    created  basic container for view control

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { Box, Typography } from "@mui/material";
 
 import "../styles/Header.css";
+import "../styles/ViewControlContainer.css";
+import ViewControlContainer from "./ViewControlContainer";
 
 const Header = () => {
   return (
     <Box id={"header"}>
       <Typography id={"header-title"}>Tâche Time</Typography>
+      <ViewControlContainer />
     </Box>
   );
 };
---
+++ client/src/components/ViewControlContainer.tsx
@@ -0,0 +1,9 @@
+import { Paper } from "@mui/material";
+
+import "../styles/ViewControlContainer.css";
+
+const ViewControlContainer = () => {
+  return <Paper id={"view-control-container"}></Paper>;
+};
+
+export default ViewControlContainer;
