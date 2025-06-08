6994ab7ccd1d94624bcd6adc5456be9cf9115d70
Jack Young <jdydev2003@gmail.com>
2025-06-07 22:01:33 -0500

    added menu button inside of view control container

--- client/src/components/ViewControlContainer.tsx
+++ client/src/components/ViewControlContainer.tsx
@@ -1,9 +1,18 @@
-import { Paper } from "@mui/material";
+import { IconButton, Paper, Tooltip } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import "../styles/ViewControlContainer.css";
 
 const ViewControlContainer = () => {
-  return <Paper id={"view-control-container"}></Paper>;
+  return (
+    <Paper id={"view-control-container"}>
+      <Tooltip title="View Menu" placement="bottom" arrow>
+        <IconButton>
+          <MenuIcon sx={{ fill: "black" }} />
+        </IconButton>
+      </Tooltip>
+    </Paper>
+  );
 };
 
 export default ViewControlContainer;
