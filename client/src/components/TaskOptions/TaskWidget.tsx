5e175cbdae1b204ac3495256cd629bf62d63eca5
dev-pigeon <141508421+dev-pigeon@users.noreply.github.com>
2025-06-10 17:57:41 -0500

    Task widget container (#30)
    * renamed task widget container to task widget
    
    * renamed style sheet
    
    * renamed component
    
    * task widget container holds task widget

---
+++ client/src/components/TaskOptions/TaskWidget.tsx
@@ -0,0 +1,34 @@
+import { IconButton, Paper, Tooltip, Collapse } from "@mui/material";
+import { useState } from "react";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+
+import "../../styles/TaskWidgetContainer.css";
+import TaskOptions from "./TaskOptions";
+
+const TaskWidget = () => {
+  const [openTaskWidget, setOpenTaskWidget] = useState<boolean>(false);
+
+  const toggleTaskWidget = () => {
+    setOpenTaskWidget(!openTaskWidget);
+  };
+
+  return (
+    <Paper id={"task-widget-container"}>
+      <Collapse in={openTaskWidget}>
+        <TaskOptions />
+      </Collapse>
+
+      <Tooltip
+        placement="left"
+        arrow
+        title={openTaskWidget ? "Close Task Options" : "Open Task Options"}
+      >
+        <IconButton className="task-widget-button" onClick={toggleTaskWidget}>
+          <AssignmentIndIcon sx={{ fill: "black", fontSize: 40 }} />
+        </IconButton>
+      </Tooltip>
+    </Paper>
+  );
+};
+
+export default TaskWidget;
--- client/src/components/TaskOptions/TaskWidgetContainer.tsx
+++ client/src/components/TaskOptions/TaskWidgetContainer.tsx
@@ -1,33 +1,11 @@
-import { IconButton, Paper, Tooltip, Collapse } from "@mui/material";
-import { useState } from "react";
-import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
-
-import "../../styles/TaskWidget.css";
-import TaskOptions from "./TaskOptions";
+import { Box } from "@mui/material";
+import TaskWidget from "./TaskWidget";
 
 const TaskWidgetContainer = () => {
-  const [openTaskWidget, setOpenTaskWidget] = useState<boolean>(false);
-
-  const toggleTaskWidget = () => {
-    setOpenTaskWidget(!openTaskWidget);
-  };
-
   return (
-    <Paper id={"task-widget-container"}>
-      <Collapse in={openTaskWidget}>
-        <TaskOptions />
-      </Collapse>
-
-      <Tooltip
-        placement="left"
-        arrow
-        title={openTaskWidget ? "Close Task Options" : "Open Task Options"}
-      >
-        <IconButton className="task-widget-button" onClick={toggleTaskWidget}>
-          <AssignmentIndIcon sx={{ fill: "black", fontSize: 40 }} />
-        </IconButton>
-      </Tooltip>
-    </Paper>
+    <Box sx={{ position: "fixed", bottom: "5%", right: "10%" }}>
+      <TaskWidget />
+    </Box>
   );
 };
 
