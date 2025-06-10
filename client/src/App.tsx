ac46ed834d94d23b2aecb8fff6f1fa61d949f998
Jack Young <jdydev2003@gmail.com>
2025-06-10 16:25:41 -0500

    removed AddTask from screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import CalendarContainer from "./components/Calendar/CalendarContainer";
 import Header from "./components/Header";
-import AddTask from "./components/TaskOptions/AddTask";
+
 import TaskWidgetContainer from "./components/TaskOptions/TaskWidgetContainer";
 import ViewControlContainer from "./components/ViewControl/ViewControlContainer";
 import "./styles/App.css";
@@ -12,7 +12,6 @@ function App() {
       <CalendarContainer />
       <ViewControlContainer />
       <TaskWidgetContainer />
-      <AddTask />
     </div>
   );
 }
