1364c012c5f73fb16221a9ccf4425b0d3b1521dc
Jack Young <jdydev2003@gmail.com>
2025-06-15 14:38:22 -0600

    made calendar container smaller

--- client/src/hooks/useCalendarContainer.ts
+++ client/src/hooks/useCalendarContainer.ts
@@ -14,7 +14,7 @@ export interface useCalendarContainerReturnProps {
 }
 
 export const getCalendarContainerHeight = () : number => {
-    return Math.round(window.innerHeight * .85);
+    return Math.round(window.innerHeight * .55);
 }
 
 const useCalendarContainer = () : useCalendarContainerReturnProps => {
