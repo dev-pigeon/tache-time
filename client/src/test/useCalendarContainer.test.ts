b71231a8089405eb941c252ac5f33990bce3396d
Jack Young <jdydev2003@gmail.com>
2025-06-15 12:13:22 -0600

    fixed tests

--- client/src/test/useCalendarContainer.test.ts
+++ client/src/test/useCalendarContainer.test.ts
@@ -1,30 +1,31 @@
 import {renderHook, act} from "@testing-library/react";
 import useCalendarContainer from "../hooks/useCalendarContainer";
+import dayjs from "dayjs";
 
 describe("initialize dates works as expected", () => {
     test("correctly outputs all 'th' dates", () => {
         const calendarContainerHook = renderHook(() => useCalendarContainer()).result;
         const expected = ["May 10th", "May 11th", "May 12th", "May 13th", "May 14th", "May 15th", "May 16th"];
-        const date = new Date(2025, 4, 10);
+         const date = dayjs("2025-05-10"); 
         act(() => {
             calendarContainerHook.current.initializeDates(date)
         })
-         expect(calendarContainerHook.current.dates).toBeDefined()
+         expect(calendarContainerHook.current.days).toBeDefined()
         for(let i = 0; i < expected.length; ++i) {
-            expect(calendarContainerHook.current.dates![i].date).toBe(expected[i])
+            expect(calendarContainerHook.current.days![i].dayStr).toBe(expected[i])
         }
     })
     
     test("can properly set `nd` `st` and `rd` dates", () => {
         const calendarContainerHook = renderHook(() => useCalendarContainer()).result;
         const expected = ["May 1st", "May 2nd", "May 3rd", "May 4th", "May 5th", "May 6th", "May 7th"];
-        const firstDate = new Date(2025, 4, 1);
+        const firstDate = dayjs("2025-05-01");
         act(() => {
             calendarContainerHook.current.initializeDates(firstDate)
         })
-         expect(calendarContainerHook.current.dates).toBeDefined()
+         expect(calendarContainerHook.current.days).toBeDefined()
         for(let i = 0; i < expected.length; ++i) {
-            expect(calendarContainerHook.current.dates![i].date).toBe(expected[i])
+            expect(calendarContainerHook.current.days![i].dayStr).toBe(expected[i])
         }
     })
 })
