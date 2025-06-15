302d99f3170716a9934eb94e6e47c976f94254d1
Jack Young <jdydev2003@gmail.com>
2025-06-15 14:16:14 -0600

    wrote api

---
+++ client/src/hooks/Api.ts
@@ -0,0 +1,23 @@
+export const buildRequestBody = (params : object) => {
+    const body = {
+        'method' : "POST",
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(params)
+    }
+    return body;
+}
+
+
+export async function sendJsonRequest(url : string, body : object) {
+    try {
+    const response = await fetch(url, body);
+    const json_response = response.json();
+    return json_response;
+    } catch(error) {
+        if(error instanceof Error) {
+            console.error("Error fetching json data: ", error);
+        }
+    }
+}
