5dfe106c28688c8129e092d6a61a36fcc88aef08
Jack Young <jdydev2003@gmail.com>
2025-06-16 16:15:57 -0600

    deleted unused imports

--- server/src/controllers/scheduleTasksController.js
+++ server/src/controllers/scheduleTasksController.js
@@ -1,11 +1,8 @@
-const Day = require("../models/Day");
-const TaskListItem = require("../models/TaskListItem");
-const TimeSlot = require("../models/TimeSlot");
 const ScheduleRequestParser = require("../models/ScheduleRequestParser");
 
 exports.scheduleTasks = (req, res) => {
   const parser = new ScheduleRequestParser();
   const requestBody = req.body;
-  parser.parseScheduleRequest(requestBody);
+  const tasks = parser.parseScheduleRequest(requestBody);
   res.status(503).json({ message: "Route not in service!" });
 };
