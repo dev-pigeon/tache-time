6180548e1be97a7fe34f78997425567635b7c410
dev-pigeon <141508421+dev-pigeon@users.noreply.github.com>
2025-06-16 09:36:15 -0600

    creted task list item model (#51)

---
+++ server/src/models/TaskListItem.js
@@ -0,0 +1,22 @@
+const dayjs = require("daysjs");
+class TaskListItem {
+  constructor(
+    title,
+    dateString,
+    dueTime,
+    estimatedTime,
+    description = "",
+    id,
+    date_iso_string
+  ) {
+    this.title = title;
+    this.dateString = dateString;
+    this.dueTime = dueTime;
+    this.estimatedTime = estimatedTime;
+    this.description = description;
+    this.id = id;
+    this.date = dayjs(date_iso_string);
+  }
+}
+
+module.exports = TaskListItem;
