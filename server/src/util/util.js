dcd81aae2f3716313c087da470a2e982d484afb3
Jack Young <jdydev2003@gmail.com>
2025-07-16 17:08:11 -0600

    removed find server port function

--- server/src/util/util.js
+++ server/src/util/util.js
@@ -1,31 +1,3 @@
-const net = require("net");
-
-function findServerPort(startPort = 3000) {
-  const MAX_PORT = 64000;
-  const invalidPort = "ERROR: Input port must be greater than 1023.";
-  const cannotFindPort = "ERROR: Cannot find valid port not in use.";
-
-  if (startPort < 1024) {
-    throw new Error(invalidPort);
-  }
-
-  for (let port = startPort; port < MAX_PORT; port++) {
-    try {
-      const server = net.createServer().listen(port);
-      server.close();
-      return port;
-    } catch (err) {
-      if (err.code === "EADDRINUSE") {
-        continue;
-      } else {
-        throw err;
-      }
-    }
-  }
-
-  throw new Error(cannotFindPort);
-}
-
 function createTaskDictionary(taskList) {
   let taskDictionary = {};
   for (const task of taskList) {
@@ -41,6 +13,5 @@ function createTaskDictionary(taskList) {
 }
 
 module.exports = {
-  findServerPort,
   createTaskDictionary,
 };
